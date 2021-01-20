a9823b8574cdf50d32442507990dc41cb246725a
DESKTOP-336SF71\HONGs <sdat789@naver.com>
2021-01-20 11:11:12 +0900

    엘리베이터 짜증..

--- Assets/scripts/elevator_move.cs
+++ Assets/scripts/elevator_move.cs
@@ -6,25 +6,23 @@ public class elevator_move : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
-    public int direction = 1;  // 1 is right 2 is left
-    private float timer = 0;
+    //public int direction = 1;  // 1 is right 2 is left
+    public int direction; // 1 = right 0 = left
+
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-
+    {   
         if (direction == 1){
             transform.position += Vector3.right * speed * Time.deltaTime;
+        }else{
+            transform.position -=  Vector3.right * speed * Time.deltaTime;
         }
-        else if (direction == 0){
-            transform.position -= Vector3.right * speed * Time.deltaTime;
-        }
-        
-        
     }
+    
 }
